4a15e960d4f98c687203ba871f40c3a1f3681747
unknown <johnrasch@gmail.com>
2013-04-24 15:32:16 -0400

    Add stored procs

---
+++ SqlScripts/10.FUNCTIONS/1.nop_getnotnullnotempty.sql
@@ -0,0 +1,19 @@
+IF OBJECT_ID('nop_getnotnullnotempty') IS NOT NULL DROP FUNCTION nop_getnotnullnotempty
+GO
+
+CREATE FUNCTION [dbo].[nop_getnotnullnotempty]
+(
+    @p1 nvarchar(max) = null, 
+    @p2 nvarchar(max) = null
+)
+RETURNS nvarchar(max)
+AS
+BEGIN
+    IF @p1 IS NULL
+        return @p2
+    IF @p1 =''
+        return @p2
+
+    return @p1
+END
+GO
